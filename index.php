71fea73269cfca07f580d40f1b418918aa451cd2
anonymframework <serif.saglam119@outlook.com>
2015-09-14 01:21:50 +0300

    made some updates

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
 
 include "vendor/autoload.php";
-
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-$table = new \Anonym\Components\Tools\Table();
-
-$table->int('aa')
-    ->int('bb')
-    ->varchar('aa')
-    ->varchar('bb')
-    ->create('deneme');
-
-
-echo $table->fetch();
