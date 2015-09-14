c39c84089f853e280e5c6b35f796b0c36660c166
anonymframework <serif.saglam119@outlook.com>
2015-09-14 03:33:03 +0300

    made some updates

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 include "vendor/autoload.php";
+
+$table = new \Anonym\Components\Tools\Table();
+
+$response = $table->varchar('aa')
+    ->int('bb')
+    ->null('NOT NULL');
+
+
+var_dump($response->fetch());
