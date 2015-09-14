6171f6defa2cdb15743f205cf1b14b498890b806
anonymframework <serif.saglam119@outlook.com>
2015-09-14 04:19:22 +0300

    made some updates

--- src/Anonym/Components/Chield.php
+++ src/Anonym/Components/Chield.php
@@ -100,7 +100,7 @@ class Chield implements TableInterface
     public function rende(){
 
         $null = $this->null === false ? 'NOT NULL': 'NULL';
-        $default = $this->default ? sprintf("DEFAULT '%s'", strval($this->default));
+        $default = $this->default ? sprintf("DEFAULT '%s'", strval($this->default)) : '';
 
         $content = call_user_func_array('sprintf', array_merge($this->pattern, $this->values));
         return $content.' '.$null.$default;
