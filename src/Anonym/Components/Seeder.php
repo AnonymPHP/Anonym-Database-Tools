7291550d04bf58500ce56ecb02d83e3a4f5489c0
anonymframework <serif.saglam119@outlook.com>
2015-09-14 14:04:23 +0300

    made some updates

--- src/Anonym/Components/Seeder.php
+++ src/Anonym/Components/Seeder.php
@@ -10,6 +10,8 @@
 
 namespace Anonym\Database\Tools\Backup;
 
+use Illuminate\Container\Container;
+
 /**
  * Class Seeder
  * @package Anonym\Database\Tools\Backup
@@ -17,7 +19,38 @@ namespace Anonym\Database\Tools\Backup;
 class Seeder
 {
 
+    /**
+     * @var string
+     */
+    protected $container;
 
+    /**
+     *
+     * @var string
+     */
+    protected $namespace = 'App\\Database\\Seeds\\';
 
+    public function __construct(Container $container = null)
+    {
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+     * @param string $container
+     * @return Seeder
+     */
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+        return $this;
+    }
 
 }
